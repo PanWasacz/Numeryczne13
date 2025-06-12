7a13be90453e8739bba3d25ba8bda00f3134f7a6
PanWasacz <michalekmroz@gmail.com>
2025-06-12 22:44:06 +0200

    Create Liniowe.h

---
+++ include/Liniowe.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+
+// Rozwiązywanie układu równań metodą Gaussa
+std::vector<double> gaussElimination(std::vector<std::vector<double>> A, std::vector<double> b);
+#pragma once
