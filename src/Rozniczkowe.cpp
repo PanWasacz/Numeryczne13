177f8662876401861471550cd01eeead63c2734a
PanWasacz <michalekmroz@gmail.com>
2025-06-13 00:22:47 +0200

    Add files via upload

--- src/Rozniczkowe.cpp
+++ src/Rozniczkowe.cpp
@@ -1,6 +1,8 @@
 #include "Rozniczkowe.h"
 #include <stdexcept>
 
+/* Wchodzi: x0, y0, x_end, h, f(x,y); wychodzi: vector wartoœci y */
+ /* Metoda Eulera - iteracyjne wyznaczanie kolejnych wartoœci rozwi¹zania ODE */
 std::vector<double> euler(double x0, double y0, double x_end, double h, std::function<double(double, double)> f) {
     if (h <= 0)
         throw std::runtime_error("Krok musi byc dodatni.");
